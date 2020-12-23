a8a75b73b14ad77a9c8d75928d5903526bf0eab0
Hyago Oliveira <hyagogow@gmail.com>
2020-12-23 14:43:03 -0300

    Add CanvasViewer component

---
+++ Runtime/UI/CanvasViewer.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+namespace ActionCode.UI
+{
+    /// <summary>
+    /// UI class for toggling visibility of a <see cref="Canvas"/> component. It requires a nested 
+    /// <see cref="Canvas"/> component (a Canvas placed as a child of another) for optimization.
+    /// <para>Nested Canvases isolate their children from their parent so a dirty child will not force a 
+    /// parent Canvas to rebuild its geometry. Therefore, some batches are saved and performance increases.</para>
+    /// </summary>
+    [RequireComponent(typeof(Canvas))]
+    public class CanvasViewer : MonoBehaviour
+    {
+        [Tooltip("The canvas component")]
+        public Canvas canvas;
+
+        /// <summary>
+        /// Whether the canvas is visible.
+        /// </summary>
+        public bool Visible
+        {
+            get => canvas.enabled;
+            set => canvas.enabled = value;
+        }
+
+        protected virtual void Reset()
+        {
+            canvas = GetComponent<Canvas>();
+        }
+
+        /// <summary>
+        /// Shows this GameObject by enabling its Canvas component.
+        /// </summary>
+        public virtual void Show()
+        {
+            Visible = true;
+        }
+
+        /// <summary>
+        /// Hides this GameObject by disabling its Canvas component.
+        /// </summary>
+        public virtual void Hide()
+        {
+            Visible = false;
+        }
+
+        /// <summary>
+        /// Toggles this GameObject visibility.
+        /// </summary>
+        public void Toggle()
+        {
+            Visible = !Visible;
+        }
+    }
+}
